e41d9ef93ef88801db7c9291aa9a08bcc8278b9f
Bartek <Barti_x@op.pl>
2021-08-25 13:21:20 +0200

    pierwsze kroki z bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBasicComponent } from './nav-basic/nav-basic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBasicComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nav-basic/nav-basic.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-basic',
+  templateUrl: './nav-basic.component.html',
+  styleUrls: ['./nav-basic.component.css']
+})
+export class NavBasicComponent{
+  active = 1;
+}
